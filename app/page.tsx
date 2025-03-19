fefdd8aecc3c24fd954913b57da4b6c0600ddeb0
cnerylozada <cnerylozada@gmail.com>
2025-03-19 08:18:08 -0500

    chore: github links

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,26 @@ import Link from "next/link";
 export default async function Home() {
   return (
     <div className="p-4">
+      <div className="mb-4">
+        <div>
+          <Link
+            href={"https://github.com/cnerylozada/fsDaap1-dapp"}
+            target="_blank"
+            className="font-bold"
+          >
+            GITHUB: fsDaap1-dapp
+          </Link>
+        </div>
+        <div>
+          <Link
+            href={"https://github.com/cnerylozada/fsDapp1-contracts"}
+            target="_blank"
+            className="font-bold"
+          >
+            GITHUB: fsDapp1-contracts
+          </Link>
+        </div>
+      </div>
       <div className="space-y-4">
         <div>
           <Link href={"/storage"} className="block p-2 border rounded-md">
