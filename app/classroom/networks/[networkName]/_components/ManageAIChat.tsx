ecdb46070e81fba9f655cbb2db0040d4f6b2461d
cnerylozada <cnerylozada@gmail.com>
2025-06-01 12:48:03 -0500

    fix: manage ai chat

--- app/classroom/networks/[networkName]/_components/ManageAIChat.tsx
+++ app/classroom/networks/[networkName]/_components/ManageAIChat.tsx
@@ -5,26 +5,31 @@ import { getSessionIdByWallet } from "@/server/classroom-factory";
 import { createSession } from "@/server/nebula";
 import { useState } from "react";
 import { prepareContractCall, sendAndConfirmTransaction } from "thirdweb";
-import { useActiveAccount } from "thirdweb/react";
-import { Account } from "thirdweb/wallets";
 import { AIChat } from "./AIChat";
+import { useCheckWalletAndNetwork } from "@/components/hooks";
+import { useParams } from "next/navigation";
+import { Account } from "thirdweb/wallets";
 
 const assingSessionIdToWalletAddress = async (
   contract: IAppContract,
-  account: Account,
+  activeAccount: Account,
   sessionId: string
 ) => {
   const tx = prepareContractCall({
     contract: getContractByChainAndAddress(contract.chainId, contract.address),
     method:
       "function assingSessionIdToWalletAddress(address _walletAddress, string memory _sessionId) external",
-    params: [account.address, sessionId],
+    params: [activeAccount.address, sessionId],
   });
-  const receipt = await sendAndConfirmTransaction({
-    transaction: tx,
-    account,
-  });
-  return receipt;
+  try {
+    await sendAndConfirmTransaction({
+      transaction: tx,
+      account: activeAccount,
+    });
+  } catch (error) {
+    console.error(`client function assingSessionIdToWalletAddress`, error);
+    return { errorMessage: (error as any)?.message };
+  }
 };
 
 export const ManageAIChat = ({
@@ -32,42 +37,57 @@ export const ManageAIChat = ({
 }: {
   appContract: IAppContract;
 }) => {
-  const activeAccount = useActiveAccount();
+  const { networkName } = useParams();
+
   const [isChatInitialized, setIsChatInitialized] = useState(false);
   const [sessionId, setSessionId] = useState("");
   const [isLoading, setIsLoading] = useState(false);
+  const [errorMessage, setErrorMessage] = useState("");
 
-  const onManageSession = async () => {
+  const {
+    isWalletConnectedToCorrectChain,
+    targetAppNetwork,
+    walletAddress,
+    activeAccount,
+  } = useCheckWalletAndNetwork(`${networkName}`);
+
+  const onManageSession = async (acccount: Account) => {
     setIsChatInitialized(true);
     setIsLoading(true);
-    let sessionId: string;
-    if (activeAccount) {
-      sessionId = await getSessionIdByWallet(
+    setErrorMessage("");
+
+    let sessionId = await getSessionIdByWallet(appContract, acccount.address);
+    setSessionId(sessionId);
+
+    if (!sessionId) {
+      sessionId = await createSession();
+      const response = await assingSessionIdToWalletAddress(
         appContract,
-        activeAccount.address
+        acccount,
+        sessionId
       );
-      if (!sessionId) {
-        sessionId = await createSession();
-        await assingSessionIdToWalletAddress(
-          appContract,
-          activeAccount,
-          sessionId
-        );
-        sessionId = await getSessionIdByWallet(
-          appContract,
-          activeAccount.address
-        );
+      if (response?.errorMessage) {
+        sessionId = "";
+        setIsChatInitialized(false);
+        setErrorMessage(response?.errorMessage);
       }
       setSessionId(sessionId);
     }
     setIsLoading(false);
   };
 
+  if (!activeAccount || !walletAddress || !isWalletConnectedToCorrectChain)
+    return (
+      <div>
+        Connect your wallet to {targetAppNetwork?.name} to perform operations
+      </div>
+    );
+
   return (
     <div>
       {isChatInitialized ? (
         <>
-          {isLoading ? (
+          {isLoading && !sessionId ? (
             <div>Loading chat ...</div>
           ) : (
             <div className="lg:w-[65%] lg:mx-auto">
@@ -80,12 +100,15 @@ export const ManageAIChat = ({
           <button
             className="p-2 bg-blue-100 rounded-md cursor-pointer"
             onClick={async () => {
-              onManageSession();
+              onManageSession(activeAccount);
             }}
           >
             Want to talk with an on-chain AI about this contract?
           </button>
         </div>
+      )}
+      {errorMessage && (
+        <div className="mt-1 text-sm text-red-700">{errorMessage}</div>
       )}
     </div>
   );
