d638142c0e46a8f386ec06101e24e5dc864e0161
cnerylozada <cnerylozada@gmail.com>
2025-06-01 12:57:42 -0500

    fix: manage ai chat

--- app/classroom/networks/[networkName]/_components/ManageAIChat.tsx
+++ app/classroom/networks/[networkName]/_components/ManageAIChat.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 "use client";
 import { getContractByChainAndAddress } from "@/contracts/client";
 import { IAppContract } from "@/contracts/settings";
